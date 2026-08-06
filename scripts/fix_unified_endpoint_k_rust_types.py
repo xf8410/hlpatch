1097f6084142effa1c68f3df301d7f80ed4900ae
xf8410 <1421506586@qq.com>
2026-08-07 06:49:21 +0800

    按补丁模板修正K域路由单一生成点

--- scripts/fix_unified_endpoint_k_rust_types.py
+++ scripts/fix_unified_endpoint_k_rust_types.py
@@ -7,8 +7,8 @@ old_call='''        k_domain_endpoint(path, &full_uri)
 new_call='''        k_domain_endpoint(&path, &full_uri)
 '''
 count=s.count(old_call)
-assert count==43, f'k_domain_call_count={count}'
-s=s.replace(old_call,new_call)
+assert count==1, f'k_domain_template_call_count={count}'
+s=s.replace(old_call,new_call,1)
 old_replace="""    let token=domain.replace('/','_');let like=format!(\"%{}%\",token);
 """
 new_replace="""    let token=domain.replace('/',\"_\");let like=format!(\"%{}%\",token);
