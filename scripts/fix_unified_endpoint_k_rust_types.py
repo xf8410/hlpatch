3b7d2d3e9232ca5cef3bbfab7d9d3323492cd009
xf8410 <1421506586@qq.com>
2026-08-07 06:46:41 +0800

    修正K域路由调用实际数量为43

--- scripts/fix_unified_endpoint_k_rust_types.py
+++ scripts/fix_unified_endpoint_k_rust_types.py
@@ -7,7 +7,7 @@ old_call='''        k_domain_endpoint(path, &full_uri)
 new_call='''        k_domain_endpoint(&path, &full_uri)
 '''
 count=s.count(old_call)
-assert count==44, f'k_domain_call_count={count}'
+assert count==43, f'k_domain_call_count={count}'
 s=s.replace(old_call,new_call)
 old_replace="""    let token=domain.replace('/','_');let like=format!(\"%{}%\",token);
 """
